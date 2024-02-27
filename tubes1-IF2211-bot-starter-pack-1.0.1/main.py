bfc44cde790f978671b8d82065c3bcc9e5f8169b
Juan A <140235896+juanaw6@users.noreply.github.com>
2024-02-28 04:51:30 +0700

    create block_density.py

--- tubes1-IF2211-bot-starter-pack-1.0.1/main.py
+++ tubes1-IF2211-bot-starter-pack-1.0.1/main.py
@@ -6,6 +6,8 @@ from game.api import Api
 from game.board_handler import BoardHandler
 from game.bot_handler import BotHandler
 from game.logic.random import RandomLogic
+from game.logic.block_density import BlockDensityLogic
+from game.logic.enrique import EnriqueLogic
 from game.util import *
 from game.logic.base import BaseLogic
 
@@ -14,6 +16,8 @@ BASE_URL = "http://localhost:3000/api"
 DEFAULT_BOARD_ID = 1
 CONTROLLERS = {
     "Random": RandomLogic,
+    "BlockDensity": BlockDensityLogic,
+    "Enrique" : EnriqueLogic
 }
 
 ###############################################################################

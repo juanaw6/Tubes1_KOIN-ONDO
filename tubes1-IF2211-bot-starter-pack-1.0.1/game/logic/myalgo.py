87817746c0ab76ddd37bed7242dd18ced15e11a4
Otzzu <mesachharmasendro@gmail.com>
2024-03-03 13:09:56 +0700

    feat: try make more modular

--- tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/myalgo.py
+++ tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/myalgo.py
@@ -1,6 +1,6 @@
 from game.logic.base import BaseLogic
 from game.models import GameObject, Board, Position, Base
-from ..util import get_direction, position_equals
+from ..util import get_direction, position_equals, clamp
 
 import random
 from typing import Optional, List
@@ -15,13 +15,41 @@ class MyAlgo(BaseLogic):
     def __init__(self):
         self.goal_position: Optional[Position] = None
         self.goal_obj: Optional[GameObject] = None
-        self.is_use_teleporter: bool = False
-        self.time_reset_teleporter: Optional[int] = None
-        self.next_position_dodge: List[Position] = []
-        self.is_dodge = False
+        # self.is_use_teleporter: bool = False
+        # self.time_reset_teleporter: Optional[int] = None
+        # self.next_position_dodge: List[Position] = []
+        # self.is_dodge = False
         # self.around = [(1, 0), (0, 1), (-1, 0), (0, -1), (1,1), (-1, -1), (1, -1), (-1, 1)]
         # self.is_attack_before = False
         # self.second_goal_position: Optional[Position] = None
+    
+    def get_direction_y(current_x, current_y, dest_x, dest_y):
+        delta_x = clamp(dest_x - current_x, -1, 1)
+        delta_y = clamp(dest_y - current_y, -1, 1)
+        if delta_y != 0:
+            delta_x = 0
+        return (delta_x, delta_y)
+    
+    def calcRange(pos_from, pos_to, teleporter1, teleporter2, range_before):
+        range_tel_1 =  abs(teleporter1.x - pos_from.x) + abs(teleporter1.y - pos_from.y)
+        range_tel_2 =  abs(teleporter2.x - pos_to.x) + abs(teleporter2.y - pos_to.y)
+        tot_range_1 = range_tel_1 + range_tel_2
+        range_tel_2 =  abs(teleporter2.x - pos_from.x) + abs(teleporter2.y - pos_from.y)
+        range_tel_1 =  abs(teleporter1.x - pos_to.x) + abs(teleporter1.y - pos_to.y)
+        tot_range_2 = range_tel_1 + range_tel_2
+        
+        if (tot_range_1 < tot_range_2):
+            if (tot_range_1 < range_before):
+                    range = tot_range_1
+                    teleporter_from = teleporter1
+                    teleporter_to = teleporter2
+            else:
+                if (tot_range_2 < range_before):
+                    range = tot_range_2
+                    teleporter_from = teleporter2
+                    teleporter_to = teleporter1 
+        
+        return (range, teleporter_from, teleporter_to)
 
     def next_move(self, board_bot: GameObject, board: Board):
         try:
@@ -46,16 +74,16 @@ class MyAlgo(BaseLogic):
 
                     teleporters_arr.append(ob) 
                     
-            if self.is_dodge:
-                print(self.next_position_dodge)
-                if len(self.next_position_dodge) > 0:
-                    pos: Position = self.next_position_dodge.pop(0)
-                    if board.is_valid_move(current_position, pos.x, pos.y):
-                        return pos.x, pos.y  
-                    else:
-                        raise Exception("invalid move")
-                else:
-                    self.is_dodge = False
+            # if self.is_dodge:
+            #     print(self.next_position_dodge)
+            #     if len(self.next_position_dodge) > 0:
+            #         pos: Position = self.next_position_dodge.pop(0)
+            #         if board.is_valid_move(current_position, pos.x, pos.y):
+            #             return pos.x, pos.y  
+            #         else:
+            #             raise Exception("invalid move")
+            #     else:
+            #         self.is_dodge = False
             
             # if board_bot.properties.diamonds != board_bot.properties.inventory_size:
             #     if self.is_attack_before:
@@ -101,22 +129,26 @@ class MyAlgo(BaseLogic):
                 teleporter_to = None
                 for teleporter_id in  teleporters.keys():
                     [teleporter1, teleporter2] = teleporters[teleporter_id]
-                    range_tel_1 =  abs(teleporter1.position.x - current_position.x) + abs(teleporter1.position.y - current_position.y)
-                    range_tel_2 =  abs(teleporter2.position.x - base.x) + abs(teleporter2.position.y - base.y)
-                    tot_range_1 = range_tel_1 + range_tel_2
-                    range_tel_2 =  abs(teleporter2.position.x - current_position.x) + abs(teleporter2.position.y - current_position.y)
-                    range_tel_1 =  abs(teleporter1.position.x - base.x) + abs(teleporter1.position.y - base.y)
-                    tot_range_2 = range_tel_1 + range_tel_2
-                    if (tot_range_1 < tot_range_2):
-                        if (tot_range_1 < range):
-                            self.goal_position = teleporter1.position
-                            self.goal_obj = teleporter1
-                            self.is_use_teleporter = True
-                    else:
-                        if (tot_range_2 < range):   
-                            self.goal_position = teleporter2.position
-                            self.goal_obj = teleporter2
-                            self.is_use_teleporter = True
+                    # range_tel_1 =  abs(teleporter1.position.x - current_position.x) + abs(teleporter1.position.y - current_position.y)
+                    # range_tel_2 =  abs(teleporter2.position.x - base.x) + abs(teleporter2.position.y - base.y)
+                    # tot_range_1 = range_tel_1 + range_tel_2
+                    # range_tel_2 =  abs(teleporter2.position.x - current_position.x) + abs(teleporter2.position.y - current_position.y)
+                    # range_tel_1 =  abs(teleporter1.position.x - base.x) + abs(teleporter1.position.y - base.y)
+                    # tot_range_2 = range_tel_1 + range_tel_2
+                    
+                    range, teleporter_from, teleporter_to = self.calcRange(current_position, base, teleporter1.position, teleporter2.position, range)
+                    # if (tot_range_1 < tot_range_2):
+                    #     if (tot_range_1 < range):
+                    #         self.goal_position = teleporter1.position
+                    #         self.goal_obj = teleporter1
+                    #         self.is_use_teleporter = True
+                    # else:
+                    #     if (tot_range_2 < range):   
+                    #         self.goal_position = teleporter2.position
+                    #         self.goal_obj = teleporter2
+                    #         self.is_use_teleporter = True
+                    self.goal_position = teleporter_from.position
+                    self.goal_obj = teleporter_from
             else:
                 diamonds: List[GameObject] = []
                 for ob in board.game_objects:
@@ -126,55 +158,71 @@ class MyAlgo(BaseLogic):
                         teleporter_from = None
                         teleporter_from_2 = None
                         teleporter_to = None
+                        teleporter_to_2 = None
                         for teleporter_id in  teleporters.keys():
                             [teleporter1, teleporter2] = teleporters[teleporter_id]
-                            range_tel_1 =  abs(teleporter1.position.x - current_position.x) + abs(teleporter1.position.y - current_position.y)
-                            range_tel_2 =  abs(teleporter2.position.x - ob.position.x) + abs(teleporter2.position.y - ob.position.y)
-                            tot_range_1 = range_tel_1 + range_tel_2
-                            range_tel_2 =  abs(teleporter2.position.x - current_position.x) + abs(teleporter2.position.y - current_position.y)
-                            range_tel_1 =  abs(teleporter1.position.x - ob.position.x) + abs(teleporter1.position.y - ob.position.y)
-                            tot_range_2 = range_tel_1 + range_tel_2
-                            if (tot_range_1 < tot_range_2):
-                                if (tot_range_1 < range):
-                                    range = tot_range_1
-                                    teleporter_from = teleporter1
-                                    teleporter_to = teleporter2
-                            else:
-                                if (tot_range_2 < range):   
-                                    range = tot_range_2
-                                    teleporter_from = teleporter2
-                                    teleporter_to = teleporter1
+                            # range_tel_1 =  abs(teleporter1.position.x - current_position.x) + abs(teleporter1.position.y - current_position.y)
+                            # range_tel_2 =  abs(teleporter2.position.x - ob.position.x) + abs(teleporter2.position.y - ob.position.y)
+                            # tot_range_1 = range_tel_1 + range_tel_2
+                            # range_tel_2 =  abs(teleporter2.position.x - current_position.x) + abs(teleporter2.position.y - current_position.y)
+                            # range_tel_1 =  abs(teleporter1.position.x - ob.position.x) + abs(teleporter1.position.y - ob.position.y)
+                            # tot_range_2 = range_tel_1 + range_tel_2
+                            
+                            r, tf, tt = self.calcRange(current_position, ob.position, teleporter1.position, teleporter2.position, range)
+                            range = r
+                            teleporter_from = tf
+                            teleporter_to = tt
+                            
+                            # if (tot_range_1 < tot_range_2):
+                            #     if (tot_range_1 < range):
+                            #         range = tot_range_1
+                            #         teleporter_from = teleporter1
+                            #         teleporter_to = teleporter2
+                            # else:
+                            #     if (tot_range_2 < range):   
+                            #         range = tot_range_2
+                            #         teleporter_from = teleporter2
+                            #         teleporter_to = teleporter1
+                            
+                            r, tf, tt = self.calcRange(board_bot.properties.base, ob.position, teleporter1.position, teleporter2.position, range_to_base)
+                            range_to_base = r
+                            teleporter_from_2 = tf
+                            teleporter_to_2 = tt
                                     
-                            range_tel_1 =  abs(teleporter1.position.x - board_bot.properties.base.x) + abs(teleporter1.position.y - board_bot.properties.base.y)
-                            range_tel_2 =  abs(teleporter2.position.x - ob.position.x) + abs(teleporter2.position.y - ob.position.y)
-                            tot_range_1 = range_tel_1 + range_tel_2
-                            range_tel_2 =  abs(teleporter2.position.x - board_bot.properties.base.x) + abs(teleporter2.position.y - board_bot.properties.base.y)
-                            range_tel_1 =  abs(teleporter1.position.x - ob.position.x) + abs(teleporter1.position.y - ob.position.y)
-                            tot_range_2 = range_tel_1 + range_tel_2
-                            if (tot_range_1 < tot_range_2):
-                                if (tot_range_1 < range_to_base):
-                                    range_to_base = tot_range_1
-                                    teleporter_from_2 = teleporter1
-                            else:
-                                if (tot_range_2 < range_to_base):   
-                                    range_to_base = tot_range_2
-                                    teleporter_from_2 = teleporter2
+                            # range_tel_1 =  abs(teleporter1.position.x - board_bot.properties.base.x) + abs(teleporter1.position.y - board_bot.properties.base.y)
+                            # range_tel_2 =  abs(teleporter2.position.x - ob.position.x) + abs(teleporter2.position.y - ob.position.y)
+                            # tot_range_1 = range_tel_1 + range_tel_2
+                            # range_tel_2 =  abs(teleporter2.position.x - board_bot.properties.base.x) + abs(teleporter2.position.y - board_bot.properties.base.y)
+                            # range_tel_1 =  abs(teleporter1.position.x - ob.position.x) + abs(teleporter1.position.y - ob.position.y)
+                            # tot_range_2 = range_tel_1 + range_tel_2
+                            # if (tot_range_1 < tot_range_2):
+                            #     if (tot_range_1 < range_to_base):
+                            #         range_to_base = tot_range_1
+                            #         teleporter_from_2 = teleporter1
+                            # else:
+                            #     if (tot_range_2 < range_to_base):   
+                            #         range_to_base = tot_range_2
+                            #         teleporter_from_2 = teleporter2
                                     
-                        diamonds.append((ob, range, teleporter_from, teleporter_to, range_to_base, teleporter_from_2))  
+                        diamonds.append((ob, range, teleporter_from, teleporter_to, range_to_base, teleporter_from_2, teleporter_to_2))  
                         
-                diamonds.sort(key=lambda x: x[1] + x[4], reverse=True)  
+                if (max_inventory_size - board_bot.properties.diamonds == 1):
+                    diamonds.sort(key=lambda x: x[1] + x[4], reverse=True)
+                else:
+                    diamonds.sort(key=lambda x: x[1], reverse=True)  
+                
                 diamond = diamonds.pop()
-                if (board_bot.properties.inventory_size - board_bot.properties.diamonds <= 1):
+                if (board_bot.properties.inventory_size - board_bot.properties.diamonds <= 1 ):
                     if (diamond[0].properties.points == 2):
                         diamond = diamonds.pop()
                 
-                # check = list(filter(lambda x: x[4] < 10, diamonds))
-                # if (len(check) > 0):
-                #     while diamond[4] > 10:
-                #         diamond = diamond.pop()
+                if (max_inventory_size - board_bot.properties.diamonds != 1):
+                    check = list(filter(lambda x: x[4] < 9, diamonds))
+                    if (len(check) > 0):
+                        while diamond[4] > 9:
+                            diamond = diamond.pop()
                         
-                # print(diamond)
-                if (board_bot.properties.milliseconds_left < (diamond[1] + diamond[4] + 1) * (board.minimum_delay_between_moves * 2)) and (board_bot.properties.diamonds >= 1):
+                if (board_bot.properties.milliseconds_left < (diamond[1] + diamond[4] + 1.3) * (1000)) and (board_bot.properties.diamonds >= 1):
                     if diamond[5] != None:
                         self.goal_position = diamond[5].position
                         self.is_use_teleporter = True
@@ -189,8 +237,7 @@ class MyAlgo(BaseLogic):
                         self.goal_position.x,
                         self.goal_position.y,
                     )
-                    # end_time = time.time()
-                    # print(end_time-start_time)
+                   
                 else:
                     if (diamond[2] != None and diamond[3] != None):
                         self.goal_position = diamond[2].position
@@ -206,40 +253,40 @@ class MyAlgo(BaseLogic):
                 self.goal_position.y,
             )
 
-            if (self.is_use_teleporter == False):
-                next_x = delta_x + current_position.x
-                next_y = delta_y + current_position.y
-                filtered_teleporter = list(filter(lambda a: a.position.x == next_x and a.position.y == next_y, teleporters_arr))
+            # if (self.is_use_teleporter == False):
+            #     next_x = delta_x + current_position.x
+            #     next_y = delta_y + current_position.y
+            #     filtered_teleporter = list(filter(lambda a: a.position.x == next_x and a.position.y == next_y, teleporters_arr))
 
-                if (len(filtered_teleporter) > 0):
-                    print("Halo cokkkkkkkkkkkkkkkkkkkkk")
-                    self.is_dodge = True
-                    if (delta_x != 0):
-                        delta_next_x = delta_x
+            #     if (len(filtered_teleporter) > 0):
+            #         print("Halo cokkkkkkkkkkkkkkkkkkkkk")
+            #         self.is_dodge = True
+            #         if (delta_x != 0):
+            #             delta_next_x = delta_x
                         
-                        if current_position.y == 0:
-                            delta_x = 0
-                            delta_y = 1
+            #             if current_position.y == 0:
+            #                 delta_x = 0
+            #                 delta_y = 1
                             
-                            self.next_position_dodge.append(Position(0, delta_next_x))
-                            self.next_position_dodge.append(Position(0, delta_next_x))
-                        else:
-                            delta_x = 0
-                            delta_y = -1
-                            self.next_position_dodge.append(Position(0, delta_next_x))
-                            self.next_position_dodge.append(Position(0, delta_next_x))
-                    elif (delta_y != 0):
-                        delta_next_y = delta_y
-                        if current_position.x == 0:
-                            delta_y = 0
-                            delta_x = 1
-                            self.next_position_dodge.append(Position(delta_next_y, 0))
-                            self.next_position_dodge.append(Position(delta_next_y, 0))
-                        else:
-                            delta_y = 0
-                            delta_x = -1
-                            self.next_position_dodge.append(Position(delta_next_y, 0))
-                            self.next_position_dodge.append(Position(delta_next_y, 0))
+            #                 self.next_position_dodge.append(Position(0, delta_next_x))
+            #                 self.next_position_dodge.append(Position(0, delta_next_x))
+            #             else:
+            #                 delta_x = 0
+            #                 delta_y = -1
+            #                 self.next_position_dodge.append(Position(0, delta_next_x))
+            #                 self.next_position_dodge.append(Position(0, delta_next_x))
+            #         elif (delta_y != 0):
+            #             delta_next_y = delta_y
+            #             if current_position.x == 0:
+            #                 delta_y = 0
+            #                 delta_x = 1
+            #                 self.next_position_dodge.append(Position(delta_next_y, 0))
+            #                 self.next_position_dodge.append(Position(delta_next_y, 0))
+            #             else:
+            #                 delta_y = 0
+            #                 delta_x = -1
+            #                 self.next_position_dodge.append(Position(delta_next_y, 0))
+            #                 self.next_position_dodge.append(Position(delta_next_y, 0))
 
             # end_time = time.time()
             # print(end_time-start_time)

e8b824652473000feef9c7ba1dc6e22644cf797e
Juan A <140235896+juanaw6@users.noreply.github.com>
2024-03-04 20:42:07 +0700

    Delete tubes1-IF2211-bot-starter-pack-1.0.1 directory

--- tubes1-IF2211-bot-starter-pack-1.0.1/run-test.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-start cmd /c "python main.py --logic BlockDensity --email=test@email.com --name=juan --password=123456 --team etimo"
-start cmd /c "python main.py --logic Enrique --email=test1@email.com --name=enrique --password=123456 --team etimo"
-start cmd /c "python main.py --logic Mesach --email=test2@email.com --name=mesach --password=123456 --team etimo"
-start cmd /c "python main.py --logic Mesach3 --email=test3@email.com --name=mesach3 --password=123456 --team etimo"
